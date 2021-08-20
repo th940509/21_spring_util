88e68b5e89d57b549f223cc1e5a8eb89866f5ec0
잉송영 <@LAPTOP-B5E5718U>
2021-08-20 15:11:43 +0900

    0820

---
+++ src/main/java/com/spring/util/tiles/TilesController.java
@@ -0,0 +1,31 @@
+package com.spring.util.tiles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/tiles")
+public class TilesController {
+
+	@RequestMapping(value = "/content1" , method = RequestMethod.GET)
+	public String content1() {
+		//tiles설정 파일의 definition 태그의 name속성의 벨류와 return값을 매칭한다.
+		return "tiles/content1";
+	}
+	
+	@RequestMapping(value = "/content2" , method = RequestMethod.GET)
+	public String content2() {
+		return "tiles/content2";
+	}
+	
+	@RequestMapping(value = "/content3" , method = RequestMethod.GET)
+	public String content3() {
+		return "tiles/content3";
+	}
+	
+	
+	
+	
+	
+}
